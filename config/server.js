a3daf76913c971058cfee8fb64d9837c82c12e8f
Vinicius Velasco <viniciusvelasco@DESKTOP-T1DOPLV.localdomain>
2020-08-19 21:08:52 -0400

    Custom left menu

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'de4bbcd1a796d687cd6751a8db829eba'),
+      secret: env("ADMIN_JWT_SECRET", "de4bbcd1a796d687cd6751a8db829eba"),
     },
+    port: 3333,
   },
 });
