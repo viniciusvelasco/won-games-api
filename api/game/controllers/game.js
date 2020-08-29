67c83a996292ce52c282bc150c2849f87a62c15c
Vinicius Velasco <vinicius.velasco@live.com>
2020-08-29 07:07:39 -0400

    Populate database with scraping GOG

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  populate: async (ctx) => {
+    console.log("Starting to populate...");
+    const options = {
+      sort: "popularity",
+      page: "1",
+      ...ctx.query,
+    };
+    await strapi.services.game.populate(options);
+
+    ctx.send("Finished populating!");
+  },
+};
